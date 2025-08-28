c31d96c17c72c28dbb0de138ecca16667a89a840
Ene-une Reuben Ochedi <54969098+RubyCodes14@users.noreply.github.com>
2025-08-28 12:22:30 +0100

    Update main.py

--- day1/section-3/eneuneochedi/main.py
+++ day1/section-3/eneuneochedi/main.py
@@ -1,4 +1,14 @@
-score = int(input("Enter your exam score (0-100): "))
+#score = int(input("Enter your exam score (0-100): "))
 
 # write your if/elif/else statements here
+
+name = input("What is your name: ")
+age = input("What is your age: ")
+interest1 = input("What is your number 1 interest: ")
+interest2 = input("What is your number 2 interest: ")
+interest3 = input("What your number 3 interest: ")
+
+combined = f"Your name is {name} and you're {age} years old. You have interest in {interest1}, {interest2} and {interest3}."
+
+print(combined)
 
