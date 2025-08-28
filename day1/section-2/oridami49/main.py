acc405cef059b40933e1de11b12aa8215d742302
damilolabrightoridami <damilolabrightoridami@gmail.com>
2025-08-28 11:55:55 +0100

    Update main.py Damilola Exercise
    Class exercise: informations about me

--- day1/section-2/oridami49/main.py
+++ day1/section-2/oridami49/main.py
@@ -1,18 +1,19 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
-    "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+    "name":"Bright-Oridami Damilola",                          # string
+    "twitter_handle": "@BrigthDamilola",                # string
+    "favorite_physics_constant": 'G' "6.67 X 10^-34",     # float
+    "age": 28,                           # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
+    "hobbies": ("Music", "Travelling"),                       # tuple
+    "skills": { 
+        ["problem solving", "critical thinking", "communication"]
+               },                        # list
+    "personal_quotes": (Live, love. laugh),               # set (one or two)
+    "contact_info": {                     # dictionary (nested)
+        "phone_number": 7088412027,              # integer (you can put a fake one)
+        "email": "oridami49@gmail.com",                     # string (you can put a fake one)
+        "website": "www.crowngracehomes.com",                   # string
     },
 }
 
 print(my_internet_profile)
-
