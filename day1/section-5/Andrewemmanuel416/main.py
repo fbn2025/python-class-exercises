9a42efb9c4832810f55aba59e1b71df75fda77d8
Andrew Emmanuel Ayodele <113485884+Emsko1@users.noreply.github.com>
2025-08-28 14:34:22 +0100

    Update main.py

--- day1/section-5/Andrewemmanuel416/main.py
+++ day1/section-5/Andrewemmanuel416/main.py
@@ -1,7 +1,7 @@
 # List of account numbers
 accounts = ["1234567890", "9876543210", "5555555555", "1111222233"]
 
-# Asks the user to enter an account number
+# Ask the user to enter an account number
 account_to_search= input("Enter account to search: ")
 
 # Flag to track if found
@@ -17,3 +17,16 @@ for acc in accounts:
 # If not found after loop
 if not found:
     print("404 - Account not found")
+
+#for the while loop
+while True:
+    account_number = input("Enter your 10-digit account number: ")
+
+    # Keep asking until a valid 10-digit numeric account number is entered
+
+    # Check if it is numeric and exactly 10 digits
+    if account_number.isdigit() and len(account_number) == 10:
+        print("Account number accepted")
+        break  # exit the loop once valid
+    else:
+        print("❌ Invalid account number. Please try again.")
