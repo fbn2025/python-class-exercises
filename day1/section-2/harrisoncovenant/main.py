ecb7ca9fdef515539f996ec3fe4eb98f0805ffd9
Covenant Harrison <117892335+Convhe@users.noreply.github.com>
2025-08-28 12:02:12 +0100

    Update main.py

--- day1/section-2/harrisoncovenant/main.py
+++ day1/section-2/harrisoncovenant/main.py
@@ -1,18 +1,17 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
+    "name": "Covenant",                          # string
+    "twitter_handle": "@ven",                # string
+    "favorite_physics_constant": "g = 9.8m/s",     # float
+    "age": 20,                           # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
+    "hobbies": ("Reading,", "Bowling,", "Swimming"),                       # tuple
+    "skills": (["Dancing", "Sewing", "Sketching"]),                        # list
+    "personal_quotes": {"Do Life Big,", "Whatever is worth doing, is worth doing well"},               # set (one or two)
     "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+        "phone_number": +234701234565,              # integer (you can put a fake one)
+        "email": "covenant@gmail.com",                     # string (you can put a fake one)
+        "website": "www.covenantprofille.com",                   # string
     },
 }
 
 print(my_internet_profile)
-
