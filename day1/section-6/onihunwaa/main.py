674f64c1090db19bbd924c8f3d1f48a00fcc7804
Onihunwa Augustine <onihunwaa@gmail.com>
2025-08-28 16:09:35 +0100

    Update main.py
    Assessment6

--- day1/section-6/onihunwaa/main.py
+++ day1/section-6/onihunwaa/main.py
@@ -1,8 +1,10 @@
+import math
+
 def find_min_max(*args):
-  min_num = float("-inf") # smallest possible number in python
-  max_num = float("inf") # largest possible number in python
-  
-  return (min_num, max_num)
+    if not args:
+        raise ValueError("At least one number is required")
+    return (min(args), max(args))
 
-# TODO: write test cases and pass them to the function. Print the results
-
+print(find_min_max(4, 7, 2, 9, 0, 3))                 
+print(find_min_max(-9, -12, 38, 489, 38, 2, -2))      
+print(find_min_max(3903, 28, math.e, -90))            
