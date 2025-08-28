2f7b22e9083f713917f45e8f18c29118b09ab580
Ene-une Reuben Ochedi <54969098+RubyCodes14@users.noreply.github.com>
2025-08-28 16:56:19 +0100

    Update main.py
    Two exercises in a file

--- day1/section-5/eneuneochedi/main.py
+++ day1/section-5/eneuneochedi/main.py
@@ -1,3 +1,6 @@
+#-------------------------------------------
+#             EXERCISE 1
+#*******************************************
 account_to_search = input("Enter account number to search: ")
 
 # TODO: use a for loop to check if search_account is in accounts
@@ -12,3 +15,20 @@ for acctNum in accounts:
         response = "404 - Account not found"
 
 print(response)
+
+#-------------------------------------------
+#             EXERCISE - 2
+#*******************************************
+prompt = "Enter your 10-digit account number: "
+
+while True:
+    account_number = input(f"{prompt}")
+
+    # TODO: check that it is all digits and has length 10
+    # If valid -> break loop, else keep asking
+
+    if account_number.isdigit() and len(account_number) == 10:
+        print("Account number accepted")
+        break
+    else:
+        prompt = "Invalid account number, enter again: "
