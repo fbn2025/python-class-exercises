20dd1e274199f204a50bdc1315d406b3632abc41
Andrew Emmanuel Ayodele <113485884+Emsko1@users.noreply.github.com>
2025-08-28 14:22:17 +0100

    Update main.py

--- day1/section-5/Andrewemmanuel416/main.py
+++ day1/section-5/Andrewemmanuel416/main.py
@@ -2,14 +2,14 @@
 accounts = ["1234567890", "9876543210", "5555555555", "1111222233"]
 
 # Asks the user to enter an account number
-search_account = input("Enter account to search: ")
+account_to_search= input("Enter account to search: ")
 
 # Flag to track if found
 found = False
 
 # Use a for loop to check if the account exists
 for acc in accounts:
-    if acc == search_account:
+    if acc == account_to_search:
         print("200 - Account found")
         found = True
         break  # stop searching once found
