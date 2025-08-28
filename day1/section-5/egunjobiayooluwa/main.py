25a3188ef5b6caa868a0253c27921126fa400d1f
Egunjobi Ayooluwa Oluwaferanmi <egunjobiayooluwa@gmail.com>
2025-08-28 14:37:02 +0100

    Update exercise 5.1 main.py

--- day1/section-5/egunjobiayooluwa/main.py
+++ day1/section-5/egunjobiayooluwa/main.py
@@ -1,3 +1,19 @@
+#EXCERCISE ONE******************************************************************************************
+accounts = ["1234567890", "9876543210", "5555555555", "1111222233"]
+
+account_to_search = input("Enter account number to search: ")
+
+# TODO: use a for loop to check if search_account is in accounts
+# Hint: loop through accounts and compare the `account_to_search` to the looped element
+
+for account in accounts:
+    if account == account_to_search:
+        print("202, account found")
+        break
+else:
+    print("404, account not found")
+
+#EXERCISE TWO*******************************************************************************************
 while True:
     account_number = input("Enter your 10-digit account number: ")
 
