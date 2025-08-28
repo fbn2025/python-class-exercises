0cfae08fa4049df73d42dea195736ef08ca3eb2c
Francis Adedeji <146173214+francisadedeji@users.noreply.github.com>
2025-08-28 12:04:12 +0100

    Update main.py

--- day1/section-2/francisadedejiayo/main.py
+++ day1/section-2/francisadedejiayo/main.py
@@ -1,18 +1,16 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
+    "name": "Francis Adedeji",                    # string
+    "twitter_handle": "gracefranca_",             # string
+    "favorite_physics_constant": 9.81,     # float
+    "age": 30,                           # integer (you can lie about this lol)
+    "finished_uni": True,                 # boolean
+    "hobbies": ("Reading", "Sleeping", "Watching movies"),                       # tuple
+    "skills": ["Python Programming", "ML/AL Analyst", "Communication", "Data Analysis"],            # list
+    "personal_quotes": {"My today better than yesterday", "Blessed be God"},               # set (one or two)
     "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+        "phone_number": 2348167554835,              # integer (you can put a fake one)
+        "email": "francisadedeji@gmail.com",                     # string (you can put a fake one)
+        "website": "www.gracefrancaword.blogspot.com",                   # string
     },
 }
-
 print(my_internet_profile)
-
