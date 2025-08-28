5bb368be80d2c2558cb341942da807e508d3e05e
Akintayo abidemi tope <akintayoabidemitope@gmail.com>
2025-08-28 07:13:43 -0700

    Add changes to exercise 5

--- day1/section-5/akintayoabidemitope/main.py
+++ day1/section-5/akintayoabidemitope/main.py
@@ -14,15 +14,15 @@ if found:
 else:
     print("Account not found.")
     
-
+/
 # Exercise: Continuously prompt the user to enter a valid 10-digit account number.
-# A valid account number should consist only of digits and be exactly 10 characters long.
+
 while True:
     account_number = input("Enter your 10-digit account number: ")
     
     # Check if the account number has only digits and length is 10
     if account_number.isdigit() and len(account_number) == 10:
-        break  # valid input, exit the loop
+        print ("Thank you! Your account number is valid.")
     else:
         print("Invalid input. Please enter exactly 10 digits.")
     # If valid -> break loop, else keep asking
