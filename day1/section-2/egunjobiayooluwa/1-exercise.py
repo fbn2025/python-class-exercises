4ea2c6622e7734d4af5911aaefaec2d8970aa6fe
Ayoose <egunjobiayooluwa@gmail.com>
2025-08-28 12:19:11 +0100

    create file 1-exercise.py

---
+++ day1/section-2/egunjobiayooluwa/1-exercise.py
@@ -0,0 +1,17 @@
+my_internet_profile = {
+    "name": "Ayooluwa Egunjobi",                          # string
+    "twitter_handle": "ayoose4",                # string
+    "favorite_physics_constant": 9.81,     # float
+    "age": 16,                           # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
+    "hobbies": ("Nothing", "Nothing", "Nothing"),                       # tuple
+    "skills": ["Java", "Jira", "Selenium"],                        # list
+    "personal_quotes": {"The day you stop learning is the day you start dying.", "Action and reaction are equal but opposite."},               # set (one or two)
+    "contact_info": {                      # dictionary (nested)
+        "phone_number": 2348162279108,              # integer (you can put a fake one)
+        "email": "egunjobiayooluwa@gmail.com",                     # string (you can put a fake one)
+        "website": "www.watchout.com",                   # string
+    },
+}
+
+print(my_internet_profile)
