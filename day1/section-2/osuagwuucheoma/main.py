4930a6b0baf8bfc528cf48cecc1a8806fd207846
JhulieeUcheoma <osuagwuucheoma@gmail.com>
2025-08-28 12:02:41 +0100

    Add my internet profile

--- day1/section-2/osuagwuucheoma/main.py
+++ day1/section-2/osuagwuucheoma/main.py
@@ -1,16 +1,16 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
+    "name": "Jhuliee Ucheoma",                          # string
+    "twitter_handle": "OsuagwuUcheoma",                # string
+    "favorite_physics_constant": 3.14159,     # float
+    "age": 35,                           # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
+    "hobbies": ("Singing", "Writing"),                       # tuple
+    "skills": ["Writing", "Talking"],                        # list
+    "personal_quotes": {"Do you. The world will adjust"},               # set (one or two)
     "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+        "phone_number": 9067660319,              # integer (you can put a fake one)
+        "email": "osuagwuucheoma@gmail.com",                     # string (you can put a fake one)
+        "website": "https://medium.com/@julieuowrites",                   # string
     },
 }
 
