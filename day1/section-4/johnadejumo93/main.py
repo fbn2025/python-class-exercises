71e5879b3a5afda188dd4d23c7bf6a88775ab8e8
John-adejumo <johnadejumo94@gmail.com>
2025-08-28 13:54:06 +0100

    add Classwork3.py
    if/elif/else

--- day1/section-4/johnadejumo93/main.py
+++ day1/section-4/johnadejumo93/main.py
@@ -1,7 +1,22 @@
-accounts = ["1234567890", "9876543210", "5555555555", "1111222233"]
+score = int(input("Enter your exam score (0-100): "))
 
-account_to_search = input("Enter account number to search: ")
+# write your if/elif/else statements here
 
-# TODO: use a for loop to check if search_account is in accounts
-# Hint: loop through accounts and compare the `account_to_search` to the looped element
+print ("Hello, how are You?")
+Score = int(input("what is your score (0 - 100): "))
 
+if Score >= 70:
+    print("your Grade is A")
+
+elif Score >= 60:
+    print("your Grade is B")
+
+elif Score >= 50:
+    print("your Grade is C")
+
+elif Score >= 40:
+    print("your Grade is D")
+
+else: 
+    print("your Grade is F")
+ 
