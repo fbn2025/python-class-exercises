920c62ee70994c4835374cd849b0e345a1c47163
Joy Obiefuna <110569953+Joyx01@users.noreply.github.com>
2025-08-28 12:14:51 +0100

    Complete exercise one

--- day1/section-2/obiefunadimz/main.py
+++ day1/section-2/obiefunadimz/main.py
@@ -1,16 +1,16 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
+    "name": "Joy Obiefuna",                          # string
+    "twitter_handle": "Joyx01",                # string
+    "favorite_physics_constant": 9.8,     # float
+    "age": 15,                           # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
+    "hobbies": ("Dancing", "Eating", "Traveling"),                       # tuple
+    "skills": ["Data visualization", "Data Modeling", "Data Manipulation"],                        # list
+    "personal_quotes": set(["If you put your mind, you can!"]),               # set (one or two)
     "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+        "phone_number": 8149533984,              # integer (you can put a fake one)
+        "email": "obimma77@gmail.com",                     # string (you can put a fake one)
+        "website": "https://www.datascienceportfol.io/joyco",                   # string
     },
 }
 
