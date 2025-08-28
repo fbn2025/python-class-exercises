fd3dee6a235d6bd82b674eaa624688ed4072cd4a
Oluwatobi3126 <159538568+Oluwatobi3126@users.noreply.github.com>
2025-08-28 11:52:14 +0100

    Update main.py

--- day1/section-2/ijeteoluwatobi/main.py
+++ day1/section-2/ijeteoluwatobi/main.py
@@ -1,16 +1,16 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
-    "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+    "name": "Oluwatobi",                   # string
+    "twitter_handle": "@GodswillIjete",    # string
+    "favorite_physics_constant": 9.81,     # float
+    "age": 30,                             # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
+    "hobbies": ("Football", "Movies"),     # tuple
+    "skills": ["Data Analysis", "Mathematics Tutoring", "SQL"],                # list
+    "personal_quotes": {"Keep pushing forward", "Math is life"},               # set (one or two)
+    "contact_info": {                       # dictionary (nested)
+        "phone_number": 36445550,           # integer (you can put a fake one)
+        "email": "oluji@example.com",       # string (you can put a fake one)
+        "website": "https://oluwatobiportfolio.com",                           # string
     },
 }
 
