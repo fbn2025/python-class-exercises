f41fb32a8dfd7aaf0703531686c2bf4a09dd5fc7
Akande Gbolahan <76651823+ayomide000@users.noreply.github.com>
2025-08-28 13:33:03 +0100

    done

--- day1/section-4/akandegbolahanayomide/main.py
+++ day1/section-4/akandegbolahanayomide/main.py
@@ -3,12 +3,12 @@ score = int(input("Enter your exam score (0-100): "))
 # write your if/elif/else statements here
 
 if score >= 70:
-    print("A")
+    print("You are doing exceptionally well. Your Grade is A")
 elif score >= 60 and score <= 69:
-    print("B")
+    print("Keep it up. Your Grade is B")
 elif score >= 50 and score <= 59:
-    print("C")
+    print("You’re making progress. Aim higher! Your Grade is C")
 elif score >= 40 and score <= 49:
-    print("D")
+    print("Needs improvement. Stay focused! Your Grade is D")
 else:
-    print("F")
+    print("Don’t give up! Work harder next time. Your Grade is F")
