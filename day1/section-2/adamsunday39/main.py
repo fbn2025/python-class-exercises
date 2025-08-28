7e75cda60b51a19b1425b9d4a6d57d7842e0381a
Adam Sunday <adamsunday39@gmail.com>
2025-08-28 11:53:40 +0100

    Update main.py

--- day1/section-2/adamsunday39/main.py
+++ day1/section-2/adamsunday39/main.py
@@ -1,18 +1,21 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
-    "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+"profile": {
+    "name": "ADAM SUNDAY",                          # string
+    "twitter_handle": "adamsunday.1",                # string
+    "favorite_physics_constant": 6.626e-34,          # float (Planck’s constant for fun)
+    "age": 99,                                       # integer
+    "finished_uni": True,                            # boolean
+    "hobbies": ("reading", "writing", "coding", "football"),   # tuple
+    "skills": ["problem solving", "public speaking", "SQL", "Python", "data analysis"],   # list
+    "personal_quotes": {
+        "Keep pushing, even when the road gets tough.",
+        "Faith and persistence always open doors."
+    },                                               # set
+    "contact_info": {                                # dictionary (nested)
+        "phone_number": +238138413441,               # string (kept as string, safer than int)
+        "email": "adsun0607@gmail.com",              # string
+        "website": "I don't have one",               # string
     },
-}
+}}
 
 print(my_internet_profile)
-
