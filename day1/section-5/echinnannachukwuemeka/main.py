5462f2b40c7155a3b76c7a4ae909c0d7450a4aaf
NNANNA CHUKWUEMEKA ECHI <echinnannachukwuemeka@gmail.com>
2025-08-28 15:41:24 +0100

    Implement account search and validation logic
    Add account search functionality and input validation.

--- day1/section-5/echinnannachukwuemeka/main.py
+++ day1/section-5/echinnannachukwuemeka/main.py
@@ -1,3 +1,19 @@
+
+accounts = ["1234567890", "9876543210", "5555555555", "1111222233"]
+    
+account_to_search = input("Enter account number to search: ")
+    
+for account in accounts:
+    if account == account_to_search:
+            print("Account found.")
+            break
+print("Account not found.")
+
+
+
+
+
+
 while True:
     account_number = input("Enter your 10-digit account number: ")
 
