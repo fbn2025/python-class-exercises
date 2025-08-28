3be7d98f0c8bdafe5dd3099f6fd9030299b27241
Blessing Isoken <148335325+Isoken-B@users.noreply.github.com>
2025-08-28 11:58:47 +0100

    Update main.py
    I updated main.py to have information about myself

--- day1/section-2/blessingisoken97/main.py
+++ day1/section-2/blessingisoken97/main.py
@@ -1,18 +1,17 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
-    "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+    "name": "Blessing Isoken",                          # string
+    "twitter_handle": "@isoken_B",                # string
+    "favorite_physics_constant": 9.81,     # float
+    "age": 18,                           # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
+    "hobbies": ("Travelling", "Cooking", "Chess"),                       # tuple
+    "skills": ["AUTOCAD", "Swimming"],                        # list
+    "personal_quotes": {"What goes around comes around"},               # set (one or two)
+    "contact_info": {                     # dictionary (nested)
+        "phone_number": 7062017788 ,              # integer (you can put a fake one)
+        "email": "blessingisoken97@gmail.com",                     # string (you can put a fake one)
+        "website": "https://www.isokenb.com"                  # string
     },
 }
 
 print(my_internet_profile)
-
