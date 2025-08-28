85d2532047b6c49f296b22f29fcf9daa54ec416f
ubarajf <ubarajf@gmail.com>
2025-08-28 12:29:59 +0100

    Update main.py

--- day1/section-3/Ubarajf/main.py
+++ day1/section-3/Ubarajf/main.py
@@ -1,4 +1,9 @@
-score = int(input("Enter your exam score (0-100): "))
+name = input("What is your name: ")
+age = int(input("How old are you? "))
+interest1 = input("What is your number 1 interest: ")
+interest2 = input("What is your number 2 interest: ")
+interest3 = input("What is your number 3 interest: ")
 
-# write your if/elif/else statements here
+combined = f"My name is {name} and I am {age}years old. My interests are; {interest1}, {interest2}, {interest3}."
 
+print(combined)
