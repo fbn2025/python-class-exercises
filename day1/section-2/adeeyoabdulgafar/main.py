576609de2a87aa154298efed3b21ea2e32595ef2
Adeeyo Abdulgafar Adekunle <45834483+tripplea95@users.noreply.github.com>
2025-08-28 11:58:10 +0100

    My Internet Profile Exe 1.py
    First exercise on python on data structure

--- day1/section-2/adeeyoabdulgafar/main.py
+++ day1/section-2/adeeyoabdulgafar/main.py
@@ -1,18 +1,17 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
-    "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+    "name": "Abdulgafar",                                                                                # string
+    "twitter_handle": "@ABDULGAFARADEEY",                                                                # string
+    "favorite_physics_constant": 9.81,                                                                 # float
+    "age": 29,                                                                                           # integer (you can lie about this lol)
+    "finished_uni": True,                                                                                  # boolean
+    "hobbies": ("Watching Football", "Playing Football", "Playing Games", "Watching Movies"),             # tuple
+    "skills": ["Graphic Design","UI Design", "Motion Graphic", "Football Analyst"],                        # list
+    "personal_quotes": set(["No Escaping Alive"]),                                                           # set (one or two)
+    "contact_info": {                                                                                      # dictionary (nested)
+        "phone_number": "08102791004",                                                                      # integer (you can put a fake one)
+        "email": "adeeyoabdulgafar@gmail.com",                                                             # string (you can put a fake one)
+        "website": "Nowebsite.com",                                                                           # string
     },
 }
 
 print(my_internet_profile)
-
