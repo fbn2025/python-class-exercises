6e1e8219ecf29c38e87e4419620754c16b91d5b1
Hilda Odein Jack <hildajack16@gmail.com>
2025-08-28 12:05:59 +0100

    Completed exercise 1

--- day1/section-2/hildajack16/main.py
+++ day1/section-2/hildajack16/main.py
@@ -1,18 +1,17 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
+    "name": "Hilda Odein Jack",                          # string
+    "twitter_handle": "heeldaah",                # string
+    "favorite_physics_constant": 6.626e-34,     # float
+    "age": 57,                           # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
+    "hobbies": ("Reading fictions by African authors", "Snuggling with my cat", "Watching movies", "Watching animal documentaries"),                   # tuple of strings
+    "skills": ["investigating", "detecting", "designing"],                        # list
+    "personal_quotes": set(["Every cloud has a silver lining"]),               # set (one or two)
     "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+        "phone_number": 8131229277,              # integer (you can put a fake one)
+        "email": "hildaodeinjack@gmail.com",                     # string (you can put a fake one)
+        "website": "lazynaijawriter.blogpost.com",                   # string
     },
 }
 
 print(my_internet_profile)
-
