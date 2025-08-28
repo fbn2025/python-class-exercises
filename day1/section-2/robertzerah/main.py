a70a615f44d0e56848897a9d92e6c7477d3bf86c
Robert Ghandi <151914156+robertGhandi@users.noreply.github.com>
2025-08-28 11:58:27 +0100

    Update main.py

--- day1/section-2/robertzerah/main.py
+++ day1/section-2/robertzerah/main.py
@@ -1,18 +1,17 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
-    "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+    "name": "Robert Ghandi",                          # string
+    "twitter_handle": "@RPharez",                # string
+    "favorite_physics_constant": 3.14159,             # float
+    "age": 29,                                         # integer (you can lie about this lol)
+    "finished_uni": True,                              # boolean
+    "hobbies": ("reading", "playing video games", "watching football"),       # tuple
+    "skills": ["live sound mixing", "programming"],                                     # list
+    "personal_quotes": {"In a gentle way, you can shake the world", "You must be the change you wish to see in the world."},  # set (one or two)
+    "contact_info": {                                  # dictionary (nested)
+        "phone_number": 9034691773,                   # integer (you can put a fake one)
+        "email": "robertzerah@gmail.com",         # string (you can put a fake one)
+        "website": "www.robertghandi.com",                   # string
     },
 }
 
 print(my_internet_profile)
-
