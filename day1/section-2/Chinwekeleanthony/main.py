d596bd68c88fefe0072188b1e44e9ec511427e0f
Chinwekele Charles Anthony <chinwekeleanthony@gmail.com>
2025-08-28 12:05:15 +0100

    Add My Internet Profile

--- day1/section-2/Chinwekeleanthony/main.py
+++ day1/section-2/Chinwekeleanthony/main.py
@@ -1,16 +1,16 @@
 my_internet_profile = {
-    "name": "Chinwekele",                    # string
-    "twitter_handle": "Uncle_stoney",        # string
-    "favorite_physics_constant": 3.14,       # float
-    "age": 205,                              # integer (you can lie about this lol)
-    "finished_uni": True,                    # boolean
+    "name": "Chinwekele",                          # string
+    "twitter_handle": "Uncle_stoney",                # string
+    "favorite_physics_constant": 3.14,     # float
+    "age": 205,                           # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
     "hobbies": ("gaming","photography"),     # tuple
     "skills": ["writing","singing"],         # list
-    "personal_quotes": ("i have a dream"),   # set (one or two)
+    "personal_quotes": {"i have a dream"},   # set (one or two)
     "contact_info": {                        # dictionary (nested)
         "phone_number":+2349013107533,       # integer (you can put a fake one)
         "email": 'Unceman@gmail.com',        # string (you can put a fake one)
-        "website": 'www.HelpmeGod.com',      # string
+        "website": 'www.HelpmeGod.com',                   # string
     },
 }
 
