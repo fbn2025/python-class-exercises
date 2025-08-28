d22f64eefa5a647c48d22a4fd7a46fa68d886838
GideonBature <infoaboutgideon@gmail.com>
2025-08-28 14:33:51 +0100

    Add account number validation exercise

--- day1/section-5/infoaboutgideon/main.py
+++ day1/section-5/infoaboutgideon/main.py
@@ -3,4 +3,10 @@ while True:
 
     # TODO: check that it is all digits and has length 10
     # If valid -> break loop, else keep asking
-
+    if len(account_number) == 10:
+        for digit in account_number:
+            if int(digit):
+                continue
+        break
+    else:
+        continue
