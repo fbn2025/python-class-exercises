85100d818c7e3e32400af5a43850fd944370504e
Danies-debug <daniellachukwu772@gmail.com>
2025-08-28 11:58:02 +0100

    Update myexcercise.py

--- day1/section-2/daniellachukwu772/main.py
+++ day1/section-2/daniellachukwu772/main.py
@@ -1,18 +1,17 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
-    "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+    "name": "daniella",                             # string
+    "twitter_handle": "daniesbaby",                   # string
+    "favorite_physics_constant":  3.14159,                 # float
+    "age": 27,                                     # integer (you can lie about this lol)
+    "finished_uni": True,                           # boolean
+    "hobbies": ("movies", "traveling") ,                      # tuple
+    "skills":   ["swimming","dancing"],                                       # list
+    "personal_quotes": {'if you want freedom prepare for war'},                    # set (one or two)
+    "contact_info": {                                  # dictionary (nested)
+     "phone_number": 9164638566,                        # integer (you can put a fake one)
+    "email": "daniellachukwu772@gmail.com",                     # string (you can put a fake one)
+    "website": "chilea.com",                             # string
     },
 }
 
 print(my_internet_profile)
-
