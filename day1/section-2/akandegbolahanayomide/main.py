90f2690c17893d5c504e4b75abcd655a14ced464
Akande Gbolahan <76651823+ayomide000@users.noreply.github.com>
2025-08-28 11:50:53 +0100

    Update main.py

--- day1/section-2/akandegbolahanayomide/main.py
+++ day1/section-2/akandegbolahanayomide/main.py
@@ -1,16 +1,16 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
+    "name": "Gbolahan Ayomide Akande",                          # string
+    "twitter_handle": "haywhy01",                # string
+    "favorite_physics_constant": 9.81,     # float
+    "age": 20,                           # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
+    "hobbies": ("Listening to Music", "Eating", "Learning new languages"),                       # tuple
+    "skills": ["Troubleshooting", "Researching"],                        # list
+    "personal_quotes": {"Do it now"},               # set (one or two)
     "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+        "phone_number": 8160589491,              # integer (you can put a fake one)
+        "email": "akandegbolahanayomideayomide@gmail.com",                     # string (you can put a fake one)
+        "website": "gbolahan-portfolio.vercel.app",                   # string
     },
 }
 
