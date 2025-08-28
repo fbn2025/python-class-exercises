dc061825fc84413f25cfccd400aa317e24be3ba4
Emmanuel Eyo <153083716+austincizar@users.noreply.github.com>
2025-08-28 17:35:00 +0100

    Exercise 6.py

--- day1/section-6/emmanueleyo023/main.py
+++ day1/section-6/emmanueleyo023/main.py
@@ -1,8 +1,20 @@
+import math
 def find_min_max(*args):
-  min_num = float("-inf") # smallest possible number in python
-  max_num = float("inf") # largest possible number in python
-  
+  min_num = float(1e309) # largest possible number in python
+  max_num = float(-1e309) # smallest possible number in python
+  for n in args:
+      if n < min_num:
+          min_num = n
+      if n > max_num:
+          max_num = n
   return (min_num, max_num)
 
 # TODO: write test cases and pass them to the function. Print the results
 
+case_1 = [4, 7, 2, 9, 0, 3]
+case_2 = [-9, -12, 38, 489, 38, 2, -2]
+case_3 = [3903, 28, math.e, -90]
+
+print(find_min_max(*case_1))
+print(find_min_max(*case_2))
+print(find_min_max(*case_3))
