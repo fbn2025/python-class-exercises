6912702ef1da0f06659b9431cd65be34fe91064f
Egunjobi Ayooluwa Oluwaferanmi <egunjobiayooluwa@gmail.com>
2025-08-28 12:26:57 +0100

    Update section1 main.py

--- day1/section-2/egunjobiayooluwa/main.py
+++ day1/section-2/egunjobiayooluwa/main.py
@@ -1,18 +1,17 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
+    "name": "Ayooluwa Egunjobi",                          # string
+    "twitter_handle": "ayoose4",                # string
+    "favorite_physics_constant": 9.81,     # float
+    "age": 16,                           # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
+    "hobbies": ("Nothing", "Nothing", "Nothing"),                       # tuple
+    "skills": ["Java", "Jira", "Selenium"],                        # list
+    "personal_quotes": {"The day you stop learning is the day you start dying.", "Action and reaction are equal but opposite."},               # set (one or two)
     "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+        "phone_number": 2348162279108,              # integer (you can put a fake one)
+        "email": "egunjobiayooluwa@gmail.com",                     # string (you can put a fake one)
+        "website": "www.watchout.com",                   # string
     },
 }
 
 print(my_internet_profile)
-
