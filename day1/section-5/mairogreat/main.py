185a1e054eef7325778d006fd6c8711f0d8f88db
Mairo Great <mairogreat@gmail.com>
2025-08-28 14:28:13 +0100

    5.4.1 & 5.5.1 Class Exercise

--- day1/section-5/mairogreat/main.py
+++ day1/section-5/mairogreat/main.py
@@ -8,3 +8,22 @@ for acc in accounts:
         break
 else:
     print("404 - Account not found, please, try again")
+
+
+# TODO: use a for loop to check if search_account is in accounts
+# Hint: loop through accounts and compare the `account_to_search` to the looped element
+
+
+#The While Loop Excercise
+
+while True:
+    account_number = input("Enter your 10-digit account number: ")
+
+    if len(account_number) == 10 and account_number.isdigit():
+        print("Account number accepted")
+        break
+    else:
+        print("Invalid account number. Please try again.")
+
+  # TODO: check that it is all digits and has length 10
+    # If valid -> break loop, else keep asking
