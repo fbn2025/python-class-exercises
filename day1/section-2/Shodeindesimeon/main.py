8fd02185acc1c91bc0b8bf916a7701e7269b2e49
Simeon Shodeinde <shodeindesimeon@gmail.com>
2025-08-28 12:16:09 +0100

    complete exercise1

--- day1/section-2/Shodeindesimeon/main.py
+++ day1/section-2/Shodeindesimeon/main.py
@@ -1,18 +1,26 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
-    "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+    "name": "Simeon",                          # string
+    
+    "twitter_handle": "Ogbeni-Simeon",       # string
+    
+    "favorite_physics_constant": 9.8,     # float
+    
+    "age": 42,            # integer (you can lie about this lol)
+    
+    "finished_uni": True,       # boolean
+    
+    "hobbies": ("sleeping",  "music", "traveling"),    # tuple
+    
+    "skills": ["dancing" "skating"],    # list
+    
+    "personal_quotes": set(["ego na the koko", "leave woman for who get money"]),  # set (one or two)
+    
+    "contact_info": {    # dictionary (nested)
+        "phone_number": 8101138382,    # integer (you can put a fake one)
+        "email": "shodeindesimeon@gmail.com",   # string (you can put a fake one)
+        "website": "ogbeni.com",     # string
     },
+
 }
 
 print(my_internet_profile)
-
