68e3daf230fc586f41fef7ae01c47283af0208c8
Gideon Akinsola <akinsolagideonn@gmail.com>
2025-08-28 15:47:05 +0100

    Completed exercise 4

--- day1/section-5/Akinsolagideonn/main.py
+++ day1/section-5/Akinsolagideonn/main.py
@@ -1,5 +1,7 @@
 # Exercise 1 - For Loop
+accounts = ["1234567890", "9876543210", "5555555555", "1111222233"]
 
+account_to_search = input("Enter account number to search: ")
 for account_number in accounts:
     if account_to_search == account_number:
         print("Account found!")
