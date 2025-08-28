dfa1531b02a3f7174096d2eabe31904d95d3cd74
Akintayo abidemi tope <akintayoabidemitope@gmail.com>
2025-08-28 03:54:58 -0700

    Add profile information

--- day1/section-2/akintayoabidemitope/main.py
+++ day1/section-2/akintayoabidemitope/main.py
@@ -1,18 +1,17 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
+    "name": "Temitope",                          # string
+    "twitter_handle": "Elijah_duyi",                # string
+    "favorite_physics_constant": 'Ohms',     # float
+    "age": '30',                           # integer (you can lie about this lol)
+    "finished_uni": 'AAUA',                  # boolean
+    "hobbies": ('Playing football video games'),                       # tuple
+    "skills": ("Agba", "coder"),                        # list
+    "personal_quotes": ('QUE SEREA SERA'),               # set (one or two)
     "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+        "phone_number": +2348146323492,              # integer (you can put a fake one)
+        "email": 'akintayoabidemitope@gmail.com',                     # string (you can put a fake one)
+        "website": 'whyyounodeyfearface.com',                   # string
     },
 }
 
 print(my_internet_profile)
-
