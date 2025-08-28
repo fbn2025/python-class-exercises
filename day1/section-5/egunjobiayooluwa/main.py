59a5d94fcc9e6dccceb8813121457e4b67ecceff
Egunjobi Ayooluwa Oluwaferanmi <egunjobiayooluwa@gmail.com>
2025-08-28 15:39:54 +0100

    Update exercise one and two into main.py

--- day1/section-5/egunjobiayooluwa/main.py
+++ day1/section-5/egunjobiayooluwa/main.py
@@ -20,3 +20,10 @@ while True:
     # TODO: check that it is all digits and has length 10
     # If valid -> break loop, else keep asking
 
+    while account_number.isdigit() == False or len(account_number) != 10:
+        print("Invalid account number. Please try again.")
+        account_number = input("Enter your 10-digit account number: ")
+
+    print("Account number accepted.")
+    break
+
