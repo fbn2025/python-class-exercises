fc4a60aa6e2895f19717d9eceb4a9689ed442ec5
PRECIOUS EZINNE EMEGHARAIBE <ezinne.emegharaibe@gmail.com>
2025-08-28 13:13:44 +0100

    Update main.py

--- day1/section-2/ezinne.emegharaibe/main.py
+++ day1/section-2/ezinne.emegharaibe/main.py
@@ -1,18 +1,15 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
-    "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+    "name": "Ezinne Emegharaibe",                                   # string
+    "twitter_handle": "@Zinnie29",                                  # string
+    "favorite_physics_constant": 6.67430e-11,                       # float
+    "age": 26,                                                      # integer (you can lie about this lol)
+    "finished_uni": "true",                                           # boolean
+    "hobbies": ("Reading", "Playing games"),                           # tuple
+    "skills": ["Python", "Data Analysis", "Machine Learning"],             # list
+    "personal_quotes": {"If you want to be successful, you have to be willing to fail"},  # set (one or two)
+    "contact_info": {                # dictionary (nested)
+        "phone_number": 8138071921,              # integer (you can put a fake one)
+        "email": "ezinne.emegharaibe@gmail.com",    # string (you can put a fake one)
+        "website": "https://ezinne.com",                   # string
     },
 }
-
-print(my_internet_profile)
-
