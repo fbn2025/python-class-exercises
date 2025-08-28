e6b6a75abaf6bbadae892d0fc16f07314a87ac9b
GideonBature <infoaboutgideon@gmail.com>
2025-08-28 11:56:40 +0100

    Adjust comment position

--- day1/section-2/infoaboutgideon/main.py
+++ day1/section-2/infoaboutgideon/main.py
@@ -1,16 +1,16 @@
 my_internet_profile = {
-    "name": "Gideon Bature",                          # string
-    "twitter_handle": "0xGideonBature",                # string
-    "favorite_physics_constant": 3.142,     # float
-    "age": 72,                           # integer (you can lie about this lol)
-    "finished_uni": True,                  # boolean
-    "hobbies": ("Coding", "Geo-Politics", "Research", "Anime"),                       # tuple
-    "skills": ["Backend", "Open-source", "Rust", "Blockchain"],                        # list
-    "personal_quotes": {"It's better to try and fail, than to try again", "Consistency birth mastery"},               # set (one or two)
-    "contact_info": {                      # dictionary (nested)
-        "phone_number": 2348168259463,              # integer (you can put a fake one)
-        "email": "infoaboutgideon@gmail.com",                     # string (you can put a fake one)
-        "website": "https://www.benelabs.com",                   # string
+    "name": "Gideon Bature",                                                                                  # string
+    "twitter_handle": "0xGideonBature",                                                                       # string
+    "favorite_physics_constant": 3.142,                                                                       # float
+    "age": 72,                                                                                                # integer (you can lie about this lol)
+    "finished_uni": True,                                                                                     # boolean
+    "hobbies": ("Coding", "Geo-Politics", "Research", "Anime"),                                               # tuple
+    "skills": ["Backend", "Open-source", "Rust", "Blockchain"],                                               # list
+    "personal_quotes": {"It's better to try and fail, than to try again", "Consistency birth mastery"},       # set (one or two)
+    "contact_info": {                                                                                         # dictionary (nested)
+        "phone_number": 2348168259463,                                                                        # integer (you can put a fake one)
+        "email": "infoaboutgideon@gmail.com",                                                                 # string (you can put a fake one)
+        "website": "https://www.benelabs.com",                                                                # string
     },
 }
 
