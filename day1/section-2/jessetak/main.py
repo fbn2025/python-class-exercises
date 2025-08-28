5249cb420bfe0658d3bab7c182161f65fd98ad0a
Jesse <105783535+EngrTakai@users.noreply.github.com>
2025-08-28 12:03:24 +0100

    Update main.py
    this changes were made to suite my personal information.

--- day1/section-2/jessetak/main.py
+++ day1/section-2/jessetak/main.py
@@ -1,18 +1,17 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
-    "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
-    },
+    "name": "Takai Jesse Abagai",                         
+    "twitter_handle": "@JesseAbagai",                
+    "favorite_physics_constant": 3.14,                      
+    "age": 26,                                  
+    "finished_uni": True,                       
+    "hobbies": ("Singing", "gaming", "Guitar"),           
+    "skills": ["Python", "JavaScript"],        
+    "personal_quotes": {"it only gets better", "There is much more to be done and achieved"},
+    "contact_info": {
+        "phone_number": 9048774744,
+        "email": "jesstakai@gmail.com",
+        "website": "https://takai.python.com",
+    }
 }
 
 print(my_internet_profile)
-
