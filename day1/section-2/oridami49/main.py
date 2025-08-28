0740867e4d697d3b7c45593e9c61c6070b65d270
damilolabrightoridami <damilolabrightoridami@gmail.com>
2025-08-28 12:11:02 +0100

    Update main.py Damilola Exercise
    Class exercise to write about me using python

--- day1/section-2/oridami49/main.py
+++ day1/section-2/oridami49/main.py
@@ -1,14 +1,11 @@
-my_internet_profile = {
-    "name":"Bright-Oridami Damilola",                          # string
+"name":"Bright-Oridami Damilola",                          # string
     "twitter_handle": "@BrigthDamilola",                # string
     "favorite_physics_constant": 'G' "6.67 X 10^-34",     # float
     "age": 28,                           # integer (you can lie about this lol)
     "finished_uni": True,                  # boolean
     "hobbies": ("Music", "Travelling"),                       # tuple
-    "skills": { 
-        ["problem solving", "critical thinking", "communication"]
-               },                        # list
-    "personal_quotes": (Live, love. laugh),               # set (one or two)
+    "skills": ["problem solving", "critical thinking", "communication"],                        # list
+    "personal_quotes": ("Live, love. laugh"),               # set (one or two)
     "contact_info": {                     # dictionary (nested)
         "phone_number": 7088412027,              # integer (you can put a fake one)
         "email": "oridami49@gmail.com",                     # string (you can put a fake one)
