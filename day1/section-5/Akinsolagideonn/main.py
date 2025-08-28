942966ed1eab11757796e2fc38832ec706e746be
Gideon Akinsola <akinsolagideonn@gmail.com>
2025-08-28 15:44:29 +0100

    Completed exercise 4

--- day1/section-5/Akinsolagideonn/main.py
+++ day1/section-5/Akinsolagideonn/main.py
@@ -1,6 +1,19 @@
+# Exercise 1 - For Loop
+
+for account_number in accounts:
+    if account_to_search == account_number:
+        print("Account found!")
+        break 
+else:
+    print("Account not found.")
+
+# Exercise 2 - While Loop
+
 while True:
     account_number = input("Enter your 10-digit account number: ")
 
-    # TODO: check that it is all digits and has length 10
-    # If valid -> break loop, else keep asking
-
+    if account_number.isdigit() and len(account_number) == 10:
+        print("Valid account number entered:", account_number)
+        break
+    else:
+        print("Invalid account number. Please try again.")
