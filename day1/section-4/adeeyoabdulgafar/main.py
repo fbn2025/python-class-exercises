6ddb1dd2dfbaf140e34f4a42d390046dcc058c42
Adeeyo Abdulgafar Adekunle <45834483+tripplea95@users.noreply.github.com>
2025-08-28 22:53:25 +0100

    Section-4 Class Exercise.py
    use a loop to check if search-account is in accounts.

--- day1/section-4/adeeyoabdulgafar/main.py
+++ day1/section-4/adeeyoabdulgafar/main.py
@@ -1,7 +1,14 @@
+# Exercise 1 - For Loop
 accounts = ["1234567890", "9876543210", "5555555555", "1111222233"]
 
 account_to_search = input("Enter account number to search: ")
 
 # TODO: use a for loop to check if search_account is in accounts
 # Hint: loop through accounts and compare the `account_to_search` to the looped element
-
+# account_is_found = False
+for account_number in accounts:
+    if account_to_search == account_number:
+        print("account_is_found")
+    else:
+        print("Not_Found")
+    break
