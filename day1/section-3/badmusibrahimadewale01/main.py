8acf1633b29507c8c5427e32c70bee116058a11a
badmusibrahimadewale01-dot <badmusibrahimadewale01@gmail.com>
2025-08-28 12:29:10 +0100

    Completed Exercise 2

--- day1/section-3/badmusibrahimadewale01/main.py
+++ day1/section-3/badmusibrahimadewale01/main.py
@@ -1,4 +1,15 @@
-score = int(input("Enter your exam score (0-100): "))
+## Collecting Inputs Class Exercise
 
-# write your if/elif/else statements here
+# Task: Write a program that collects your name and interests
+# Goal: To teach you how to implement data collection and casting
 
+
+name = input("What is your name: ")
+age = int(input("How old are you? "))
+interest1 = input("What is your number 1 interest: ")
+interest2 = input("What is your number 2 interest: ")
+interest3 = input("What is your number 3 interest: ")
+
+combined = f"Your name is {name} and you are {age} years old. Your interests are {interest1}, {interest2}, and {interest3}."
+
+print(combined)
