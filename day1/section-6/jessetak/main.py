1a9910b220f4c25a026da400de3019288212c0bf
Jesse <105783535+EngrTakai@users.noreply.github.com>
2025-08-28 16:54:38 +0100

    Update main.py

--- day1/section-6/jessetak/main.py
+++ day1/section-6/jessetak/main.py
@@ -1,8 +1,8 @@
 def find_min_max(*args):
-  min_num = float("-inf") # smallest possible number in python
-  max_num = float("inf") # largest possible number in python
+  min_num = min(args) # smallest possible number in python
+  max_num = max(args) # largest possible number in python
   
-  return (min_num, max_num)
+  print ((min_num, max_num))
 
+find_min_max(-9, -100, 38, 450, 38, 2, -1)
 # TODO: write test cases and pass them to the function. Print the results
-
