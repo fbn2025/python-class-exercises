2ca0b79a8dd0cfceb15c9d53b1c218396da35433
RelyOnGod <148478541+Tomi-Dev1@users.noreply.github.com>
2025-08-29 03:39:44 +0100

    Update main.py

--- day1/section-4/similoluwaoluwatomisin/main.py
+++ day1/section-4/similoluwaoluwatomisin/main.py
@@ -1,10 +1,10 @@
 # Exercise 1 - For Loop
+# TODO: use a for loop to check if search_account is in accounts
+# Hint: loop through accounts and compare the `account_to_search` to the looped element
+
 accounts = ["1234567890", "9876543210", "5555555555", "1111222233"]
 
 account_to_search = input("Enter account number to search: ")
-
-# TODO: use a for loop to check if search_account is in accounts
-# Hint: loop through accounts and compare the `account_to_search` to the looped element
 
 found = False
 for account in accounts:
