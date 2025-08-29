354f245fbda32b7e0d314c0cc79158c29c24d5c0
GideonBature <infoaboutgideon@gmail.com>
2025-08-29 15:55:37 +0100

    Removed unit tests for exercise 6

--- day1/section-6/infoaboutgideon/main.py
+++ day1/section-6/infoaboutgideon/main.py
@@ -10,7 +10,6 @@
 # [4, 7, 2, 9, 0, 3]
 # [-9, -12, 38, 489, 38, 2, -2]
 # [3903, 28, math.e, -90]
-import unittest
 import math
 
 
@@ -32,38 +31,6 @@ def find_min_max(*args):
   return (min_num, max_num)
 
 # TODO: write test cases and pass them to the function. Print the results
-
-# --- Test Cases ---
-class TestMaxMin(unittest.TestCase):
-    """Tests for the find_min_max function."""
-
-    def test_no_argument(self):
-        self.assertEqual(find_min_max(), (float("-inf"), float("inf")))
-
-    def test_all_zero_arguments(self):
-        self.assertEqual(find_min_max(0, 0, 0, 0, 0), (0, 0))
-
-    def test_single_positive_argument(self):
-        self.assertEqual(find_min_max(8), (8, 8))
-
-    def test_single_negative_argument(self):
-        self.assertEqual(find_min_max(-4), (-4, -4))
-
-    def test_no_negative_arguments_with_zero(self):
-        self.assertEqual(find_min_max(4, 7, 2, 9, 0, 3), (0, 9))
-
-    def test_no_negative_arguments_without_zero(self):
-        self.assertEqual(find_min_max(4, 7, 2, 9, 5, 3), (2, 9))
-
-    def test_with_negative_arguments(self):
-        self.assertEqual(find_min_max(-9, -12, 38, 489, 38, 2, -2), (-12, 489))
-
-    def test_with_all_negative_arguments(self):
-        self.assertEqual(find_min_max(-9, -12, -38, -489, -38, -2, -76), (-489, -2))
-
-    def test_with_exponential_argument(self):
-        self.assertEqual(find_min_max(3903, 28, math.e, -90), (-90, 3903))
-
-# --- Test Runner ---
-if __name__ == '__main__':
-    unittest.main()
+print(find_min_max(4, 7, 2, 9, 0, 3))
+print(find_min_max(-9, -12, 38, 489, 38, 2, -2))
+print(find_min_max(3903, 28, math.e, -90))
