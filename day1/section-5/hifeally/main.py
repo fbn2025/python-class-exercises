729f336354710a012f4a22f8ba5267d91466826b
Kazan'an <104999366+Vichally20@users.noreply.github.com>
2025-08-29 10:59:15 +0100

    Uncommented for loop to search account number

--- day1/section-5/hifeally/main.py
+++ day1/section-5/hifeally/main.py
@@ -1,16 +1,16 @@
 # Exercise 1 - For Loop
-# accounts = ["1234567890", "9876543210", "5555555555", "1111222233"]
+accounts = ["1234567890", "9876543210", "5555555555", "1111222233"]
 
-# account_to_search = input("Enter account number to search: ")
+account_to_search = input("Enter account number to search: ")
 
-# # TODO: use a for loop to check if search_account is in accounts
-# # Hint: loop through accounts and compare the `account_to_search` to the looped element
-# for i in range(4):
-#     if accounts[i] == account_to_search:
-#         print("200 \nAccount found")
-#         break
-# else:
-#     print("404 \nAccount not found")
+# TODO: use a for loop to check if search_account is in accounts
+# Hint: loop through accounts and compare the `account_to_search` to the looped element
+for i in range(4):
+    if accounts[i] == account_to_search:
+        print("200 \nAccount found")
+        break
+else:
+    print("404 \nAccount not found")
 
 
 # part 2
