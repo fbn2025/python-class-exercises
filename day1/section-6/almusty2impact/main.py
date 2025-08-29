e545f59c861dfece374b176e054f8afead4e80ad
Almusty2Impact <almusty2impact@gmail.com>
2025-08-29 16:01:43 +0100

    Section 6, Exercise

--- day1/section-6/almusty2impact/main.py
+++ day1/section-6/almusty2impact/main.py
@@ -10,7 +10,7 @@
 # [4, 7, 2, 9, 0, 3]
 # [-9, -12, 38, 489, 38, 2, -2]
 # [3903, 28, math.e, -90]
-import unittest
+
 import math
 
 
@@ -34,36 +34,6 @@ def find_min_max(*args):
 # TODO: write test cases and pass them to the function. Print the results
 # --- Test Cases ---
 
-class TestMaxMin(unittest.TestCase):
-    """Tests for the find_min_max function."""
-
-    def test_no_argument(self):
-        self.assertEqual(find_min_max(), (float("-inf"), float("inf")))
-
-    def test_all_zero_arguments(self):
-        self.assertEqual(find_min_max(0, 0, 0, 0, 0), (0, 0))
-
-    def test_single_positive_argument(self):
-        self.assertEqual(find_min_max(8), (8, 8))
-
-    def test_single_negative_argument(self):
-        self.assertEqual(find_min_max(-4), (-4, -4))
-
-    def test_no_negative_arguments_with_zero(self):
-        self.assertEqual(find_min_max(4, 7, 2, 9, 0, 3), (0, 9))
-
-    def test_no_negative_arguments_without_zero(self):
-        self.assertEqual(find_min_max(4, 7, 2, 9, 5, 3), (2, 9))
-
-    def test_with_negative_arguments(self):
-        self.assertEqual(find_min_max(-9, -12, 38, 489, 38, 2, -2), (-12, 489))
-
-    def test_with_all_negative_arguments(self):
-        self.assertEqual(find_min_max(-9, -12, -38, -489, -38, -2, -76), (-489, -2))
-
-    def test_with_exponential_argument(self):
-        self.assertEqual(find_min_max(3903, 28, math.e, -90), (-90, 3903))
-
-#--- Test Runner ---
-if __name__ == '__main__':
-  unittest.main()
+print(find_min_max(4, 7, 2, 9, 0, 3)) # (0, 9)
+print(find_min_max(-9, -12, 38, 489, 38, 2, -2)) # (-12, 489)
+print(find_min_max(3903, 28, math.e, -90)) # (-90, 3903)
