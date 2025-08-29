074f2af5df02321d6f32b9f147cdac44943a100c
Marachi-June <ibehamarachi18@gmail.com>
2025-08-29 13:19:26 +0100

    Completed Exercise-7.py

--- day1/section-7/ibehamarachi18/main.py
+++ day1/section-7/ibehamarachi18/main.py
@@ -1,19 +1,22 @@
 class CalculatorHistory:
     def __init__(self):
-        # add the remaining
         self.addition = []
+        self.subtraction = []
+        self.division = []
+        self.multiplication = []
 
     def __str__(self):
         return f"""
-            Addition: {self.addition}
+        Addition: {self.addition}
+        Subtraction: {self.subtraction}
+        Division: {self.division}
+        Multiplication: {self.multiplication}
         """
 
 class Calculator:
     def __init__(self, log_results: bool = False):
         self.log_results = log_results
-        self.history = {
-            "addition": []
-        }
+        self.history = CalculatorHistory() 
 
     def add(self, num1: float, num2: float):
         result = num1 + num2
@@ -21,18 +24,38 @@ class Calculator:
         if self.log_results:
             print(f"The sum of {num1} and {num2} is {result}")
 
-        self.history["addition"].append((num1, num2, result))
-        # goal 
-        # self.history.addition.append((num1, num2, result))
+        # dictionary accessor
+        #self.history["addition"].append((num1, num2, result))
+
+        self.history.addition.append((num1, num2, result)) 
 
         return result
+    
+    def subtract(self, num1, num2):
+        difference = num1 - num2
+        self.history.subtraction.append((num1, num2, difference)) 
+
+        return difference
+    
+    def multiplication(self, num1, num2):
+        product = num1 * num2
+        self.history.multiplication.append((num1, num2, product)) 
+
+        return product
+    
+    def division(self, num1, num2):
+        divide = num1 / num2
+        self.history.division.append((num1, num2, divide)) 
+
+        return divide
 
 
+# Example usage
 calculator = Calculator()
 
 calculator.add(4, 5)
-calculator.add(20, 25)
-calculator.add(-20, 30)
+calculator.subtract(25, 20)
+calculator.multiplication(-20, 30)
+calculator.division(20, 20)
 
-print(calculator.history)
-
+print("The calculator history is", calculator.history)
