6c3cceb016758a2cb50d43ee20daa26b41c894cc
Godsent David Onwuteaka <onwuteakadavid@gmail.com>
2025-08-29 15:30:06 +0100

    Update main.py

--- day1/section-5/onwuteakadavid/main.py
+++ day1/section-5/onwuteakadavid/main.py
@@ -1,6 +1,12 @@
-while True:
+not_correct = True 
+while not_correct:
     account_number = input("Enter your 10-digit account number: ")
 
     # TODO: check that it is all digits and has length 10
     # If valid -> break loop, else keep asking
 
+    if len(account_number) == 10: 
+        print("Account number accepted")
+        not_correct = False 
+    else: 
+        account_number = input("Enter your 10-digit account number: ") 
