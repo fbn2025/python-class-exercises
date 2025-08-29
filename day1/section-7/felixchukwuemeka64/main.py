891fb5810b6c2db272fe2394162fe3de93c45063
Felix Chukwuemeka <34812322+Chukwuemeka2000@users.noreply.github.com>
2025-08-29 16:03:01 +0100

    Update main.py

--- day1/section-7/felixchukwuemeka64/main.py
+++ day1/section-7/felixchukwuemeka64/main.py
@@ -1,33 +1,23 @@
-class CalculatorHistory:
-    def __init__(self):
-        # add the remaining
-        self.addition = []
-
-    def __str__(self):
-        return f"""
-            Addition: {self.addition}
-        """
-
 class Calculator:
-    def __init__(self, log_results: bool = False):
-        self.log_results = log_results
-        self.history = {
-            "addition": []
-        }
-
-    def add(self, num1: float, num2: float):
-        result = num1 + num2
-
-        if self.log_results:
-            print(f"The sum of {num1} and {num2} is {result}")
-
-        self.history["addition"].append((num1, num2, result))
-        # goal 
-        # self.history.addition.append((num1, num2, result))
-
-        return result
-
-
+  def __init__(self, log_results: bool = False):
+    self.log_results = log_results
+    self.history = {
+      "addition": [],
+      "subtraction": [],
+      "division": [],
+      "multiplication": []
+    }
+  
+  def add(self, num1: float, num2: float):
+    result = num1 + num2
+    
+    if self.log_results:
+      print(f"The sum of {num1} and {num2} is {result}")
+      
+    self.history["addition"].append((num1, num2, result))
+      
+    return result
+  
 calculator = Calculator()
 
 calculator.add(4, 5)
