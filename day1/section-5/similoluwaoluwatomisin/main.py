410929a8b19d796e0bfd284d9c061a9bac74fa3f
RelyOnGod <148478541+Tomi-Dev1@users.noreply.github.com>
2025-08-29 03:38:11 +0100

    Update main.py

--- day1/section-5/similoluwaoluwatomisin/main.py
+++ day1/section-5/similoluwaoluwatomisin/main.py
@@ -4,3 +4,9 @@ while True:
     # TODO: check that it is all digits and has length 10
     # If valid -> break loop, else keep asking
 
+    if account_number.isdigit() and len(account_number) == 10:
+        print("Valid account number.")
+        break
+    else:
+        print("Invalid account number. Please try again.")  
+        
