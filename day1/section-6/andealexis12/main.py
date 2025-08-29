22e514a1b35f0a4cccb9bf73f773d233fc44a12f
Andealexis <141722383+Andealexis@users.noreply.github.com>
2025-08-29 16:18:51 +0100

    Update main.py
    Just a little update on the code

--- day1/section-6/andealexis12/main.py
+++ day1/section-6/andealexis12/main.py
@@ -6,7 +6,3 @@ def find_min_max(*args):
     max_num = max(args)
 
     return min(args), max(args)
-
-print(find_min_max(4, 7, 2, 9, 0, 3))         
-print(find_min_max(-9, -12, 38, 489, 38, 2, -2))  
-print(find_min_max(3903, 28, math.e, -90)) 
