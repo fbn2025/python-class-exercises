46471938213cba94cdd9334fd2e9b52482c4a8d8
PRECIOUS EZINNE EMEGHARAIBE <ezinne.emegharaibe@gmail.com>
2025-08-29 06:09:23 +0100

    Update main.py

--- day1/section-6/ezinne.emegharaibe/main.py
+++ day1/section-6/ezinne.emegharaibe/main.py
@@ -1,8 +1,37 @@
+# Write a function that finds the min and max number in a list of numbers
+
+# Your function should:
+
+# 1. Take several inputs as arguments find_min_max(arg1, arg2, arg3, …, argn)
+# 2. Finds the minimum and maximum number
+# 3. Returns both of them as a tuple (minimum, maximum)
+
+# Write a few cases to verify that it works:
+# [4, 7, 2, 9, 0, 3]
+# [-9, -12, 38, 489, 38, 2, -2]
+# [3903, 28, math.e, -90]
+import math
+
 def find_min_max(*args):
   min_num = float("-inf") # smallest possible number in python
   max_num = float("inf") # largest possible number in python
+
+  for number in args:
+    if number > min_num:
+      min_num = number
+    if number < max_num:
+      max_num = number
+
+  min_num, max_num = max_num, min_num
   
   return (min_num, max_num)
 
 # TODO: write test cases and pass them to the function. Print the results
+
+result1 = find_min_max(4, 7, 2, 9, 0, 3)
+print(result1)
+result2 = find_min_max(-9, -12, 38, 489, 38, 2, -2)
+print(result2)
+result3 = find_min_max(3903, 28, math.e, -90)
+print(result3)
 
