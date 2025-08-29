4be5f07de8d20eab1b1888406d4d70ad96755899
Felix Chukwuemeka <34812322+Chukwuemeka2000@users.noreply.github.com>
2025-08-29 15:57:51 +0100

    Update main.py

--- day1/section-5/felixchukwuemeka64/main.py
+++ day1/section-5/felixchukwuemeka64/main.py
@@ -3,4 +3,8 @@ while True:
 
     # TODO: check that it is all digits and has length 10
     # If valid -> break loop, else keep asking
-
+    if account_number.isdigit() and len(account_number) == 10:
+        print("Account number accepted")
+        break
+    else:
+        print("Invalid account number. Please try again.")
