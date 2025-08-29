39f5d4c8bd7da208e1b1f571d0034dff6ce21770
OBASANMI EUNICE <79628279+Beetee-tech@users.noreply.github.com>
2025-08-29 03:17:06 +0100

    Update main.py

--- day1/section-2/euniceobasanmi/main.py
+++ day1/section-2/euniceobasanmi/main.py
@@ -1,11 +1,11 @@
 my_internet_profile = {
     "name": "Obasanmi Tofunmi",                          # string
     "twitter_handle": "Tofunmi_Oba",                # string
-    "favorite_physics_constant":  ,     # float
+    "favorite_physics_constant":  6.1, # float
     "age": 20,                           # integer (you can lie about this lol)
-    "finished_uni": "Yes",                  # boolean
+    "finished_uni": "True",                  # boolean
     "hobbies":("Sewing","Reading"),                        # tuple
-    "skills": (["Sewing","Singing"]),                        # list
+    "skills": ["Sewing","Singing"],                        # list
     "personal_quotes": {"Live beyond yourself"},                # set (one or two)
     "contact_info": {                      # dictionary (nested)
         "phone_number": +2347032263835,              # integer (you can put a fake one)
