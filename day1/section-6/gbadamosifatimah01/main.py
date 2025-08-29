43436b43f0ffa5be63fd28279f7ab4ab997eb3f3
Fatimah Gbadamosi <125892812+Fateeemah01@users.noreply.github.com>
2025-08-29 09:45:51 +0100

    Update main.py

--- day1/section-6/gbadamosifatimah01/main.py
+++ day1/section-6/gbadamosifatimah01/main.py
@@ -1,11 +1,3 @@
-def find_min_max(*args):
-  min_num = float("-inf") # smallest possible number in python
-  max_num = float("inf") # largest possible number in python
-  
-  return (min_num, max_num)
-  
-
-# TODO: write test cases and pass them to the function. Print the results
 
 import math
 def find_min_max(*numbers):
@@ -39,9 +31,7 @@ print(find_min_max(4, 7, 2, 9, 0, 3))
 print(find_min_max(-9, -12, 38, 489, 38, 2, -2))
 print(find_min_max(3903, 28, math.e, -90))
 
-print("My samples cases:")
-print(find_min_max(2, 3, 28, 927, -1087, -2, 0, 91, -365))
-print(find_min_max(-2, 0, 2, -8, 57, 34, 75, 1, -21, 32, -1))
+
 
 
 
