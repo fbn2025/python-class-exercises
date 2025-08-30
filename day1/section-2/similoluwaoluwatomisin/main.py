9827dcbf7ea8e083f1c5e628c3957519fdd85c1c
RelyOnGod <148478541+Tomi-Dev1@users.noreply.github.com>
2025-08-30 12:33:14 +0100

    Update main.py

--- day1/section-2/similoluwaoluwatomisin/main.py
+++ day1/section-2/similoluwaoluwatomisin/main.py
@@ -20,12 +20,4 @@ my_internet_profile = {
 
 print(my_internet_profile)
 
-name = input("What is your name: ")
-age = input("What is your age: ")
-interest1 = input("What is your number 1 interest: ")
-interest2 = input("What is your number 2 interest: ")
-interest3 = input("What is your number 3 interest: ")
 
-combined = f"My name is {name} and I am {age} and my interests are {interest1[0]}, {interest2[1]}, and {interest3[2]}"
-
-print(combined)
