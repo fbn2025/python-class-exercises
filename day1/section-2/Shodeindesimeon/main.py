50462c4757592ff7cc7a6342db1cbffd59eea474
Simeon Shodeinde <shodeindesimeon@gmail.com>
2025-08-30 20:27:56 +0100

    complete exercise1

--- day1/section-2/Shodeindesimeon/main.py
+++ day1/section-2/Shodeindesimeon/main.py
@@ -1,24 +1,16 @@
 my_internet_profile = {
-    "name": "Simeon",                          # string
-    
-    "twitter_handle": "Ogbeni-Simeon",       # string
-    
-    "favorite_physics_constant": 9.8,     # float
-    
-    "age": 42,            # integer (you can lie about this lol)
-    
-    "finished_uni": True,       # boolean
-    
-    "hobbies": ("sleeping",  "music", "traveling"),    # tuple
-    
-    "skills": ["dancing" "skating"],    # list
-    
-    "personal_quotes": set(["ego na the koko", "leave woman for who get money"]),  # set (one or two)
-    
-    "contact_info": {    # dictionary (nested)
-        "phone_number": 8101138382,    # integer (you can put a fake one)
-        "email": "shodeindesimeon@gmail.com",   # string (you can put a fake one)
-        "website": "ogbeni.com",     # string
+    "name": "Simeon",                                                             # string
+    "twitter_handle": "Ogbeni-Simeon",                                            # string
+    "favorite_physics_constant": 9.8,                                             # float
+    "age": 42,                                                                    # integer (you can lie about this lol)
+    "finished_uni": True,                                                         # boolean
+    "hobbies": ("sleeping",  "music", "traveling"),                               # tuple
+    "skills": ["dancing" "skating"],                                              # list
+    "personal_quotes": set(["ego na the koko", "leave woman for who get money"]), # set (one or two)
+    "contact_info": {                                                             # dictionary (nested)
+    "phone_number": 8101138382,                                                   # integer (you can put a fake one)
+    "email": "shodeindesimeon@gmail.com",                                         # string (you can put a fake one)
+    "website": "ogbeni.com",                                                      # string
     },
 
 }
