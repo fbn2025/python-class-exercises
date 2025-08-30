1740918a93d95d96737cc3366d6c9ce83066028f
moyindanisi <moyindanisi@gmail.com>
2025-08-30 12:00:22 +0100

    Update main.py
    Calculator history

--- day1/section-7/moyindanisi/main.py
+++ day1/section-7/moyindanisi/main.py
@@ -1,19 +1,22 @@
 class CalculatorHistory:
     def __init__(self):
-        # add the remaining
         self.addition = []
+        self.subtraction = []
+        self.division = []
+        self.multiplication = []
 
     def __str__(self):
         return f"""
             Addition: {self.addition}
+            Subtraction: {self.subtraction}
+            Division: {self.division}
+            Multiplication: {self.multiplication}
         """
 
 class Calculator:
     def __init__(self, log_results: bool = False):
         self.log_results = log_results
-        self.history = {
-            "addition": []
-        }
+        self.history = CalculatorHistory()
 
     def add(self, num1: float, num2: float):
         result = num1 + num2
@@ -21,18 +24,51 @@ class Calculator:
         if self.log_results:
             print(f"The sum of {num1} and {num2} is {result}")
 
-        self.history["addition"].append((num1, num2, result))
-        # goal 
-        # self.history.addition.append((num1, num2, result))
+        self.history.addition.append((num1, num2, result))
 
         return result
+    
+    def subtract(self, num1: float, num2: float):
+        difference = num1 - num2
+
+        if self.log_results:
+            print(f"The difference between {num1} and {num2} is {difference}")
+
+        self.history.subtraction.append((num1, num2, difference))
+
+        return difference
+    
+    def multiply(self, num1: float, num2: float):
+        product = num1 * num2
+
+        if self.log_results:
+            print(f"The product of {num1} and {num2} is {product}")
+
+        self.history.multiplication.append((num1, num2, product))
+
+        return product
+    
+    def divide(self, num1: float, num2: float):
+        if num2 == 0:
+            raise ValueError("Cannot divide by zero.")
+        quotient = num1 / num2
+        if self.log_results:
+            print(f"The quotient of {num1} and {num2} is {quotient}")
+        self.history.division.append((num1, num2, quotient))
+        return quotient
+    
 
 
 calculator = Calculator()
 
 calculator.add(4, 5)
-calculator.add(20, 25)
-calculator.add(-20, 30)
+calculator.add(44, 5)
+calculator.subtract(20, 25)
+calculator.subtract(50, 20)
+calculator.multiply(20, 30)
+calculator.multiply(5, 30)
+calculator.divide(100, 10)
+calculator.divide(50, 2)
 
-print(calculator.history)
 
+print("The calculator history is", calculator.history)
