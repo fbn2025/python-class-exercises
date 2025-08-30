16d41f5d667609630fb78684ac895caef1e07dfa
badmusibrahimadewale01-dot <badmusibrahimadewale01@gmail.com>
2025-08-30 08:51:09 +0100

    Completed Exercise 2

--- day1/section-3/badmusibrahimadewale01/main.py
+++ day1/section-3/badmusibrahimadewale01/main.py
@@ -1,12 +1,9 @@
-score = float(input("Enter your exam score (0-100): "))
+name = input("What is your name: ")
+age = int(input("How old are you? "))
+interest1 = input("What is your number 1 interest: ")
+interest2 = input("What is your number 2 interest: ")
+interest3 = input("What is your number 3 interest: ")
 
-if score >= 70:
-    print("A")
-elif score >= 60:
-    print("B")
-elif score >= 50:
-    print("C")
-elif score >= 40:
-    print("D")
-else:
-    print("F")
+combined = f"Your name is {name} and you are {age} years old. Your interests are {interest1}, {interest2}, and {interest3}."
+
+print(combined)
