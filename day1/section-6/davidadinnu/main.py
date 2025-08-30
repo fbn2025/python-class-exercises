82e4c1a7af59b7884d3c7aa1ec6addfce8685e3d
Emmanuel Adinnu <31533087+Jobelynz@users.noreply.github.com>
2025-08-30 18:12:24 +0100

    Update main.py

--- day1/section-6/davidadinnu/main.py
+++ day1/section-6/davidadinnu/main.py
@@ -5,10 +5,4 @@ import math
 def find_min_max(*args):
       min_num = min(args) # smallest possible number in python
       max_num = max(args) # largest possible number in python
-      print((min_num, max_num))
-     
-
-find_min_max(4, 7, 2, 9, 0, 3)
-find_min_max(-9, -12, 38, 489, 38, 2, -2)
-find_min_max(3903, 28, math.e, -90)
-
+      return (min_num, max_num)
