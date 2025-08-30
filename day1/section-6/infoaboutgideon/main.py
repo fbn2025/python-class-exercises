112433a0047373ca67ace720d2d5e355165e7af6
GideonBature <infoaboutgideon@gmail.com>
2025-08-30 09:30:02 +0100

    Comment out all print statements

--- day1/section-6/infoaboutgideon/main.py
+++ day1/section-6/infoaboutgideon/main.py
@@ -10,8 +10,8 @@
 # [4, 7, 2, 9, 0, 3]
 # [-9, -12, 38, 489, 38, 2, -2]
 # [3903, 28, math.e, -90]
-import math
 
+import math
 
 def find_min_max(*args):
   min_num = float("-inf") # smallest possible number in python
@@ -31,6 +31,6 @@ def find_min_max(*args):
   return (min_num, max_num)
 
 # TODO: write test cases and pass them to the function. Print the results
-print(find_min_max(4, 7, 2, 9, 0, 3))
-print(find_min_max(-9, -12, 38, 489, 38, 2, -2))
-print(find_min_max(3903, 28, math.e, -90))
+# print(find_min_max(4, 7, 2, 9, 0, 3))
+# print(find_min_max(-9, -12, 38, 489, 38, 2, -2))
+# print(find_min_max(3903, 28, math.e, -90))
