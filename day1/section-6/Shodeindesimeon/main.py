c2a6dd2c889f4241f9339850615d2a0b28c69208
Simeon Shodeinde <shodeindesimeon@gmail.com>
2025-08-30 11:34:52 +0100

    Complete Exercise 5

--- day1/section-6/Shodeindesimeon/main.py
+++ day1/section-6/Shodeindesimeon/main.py
@@ -14,10 +14,10 @@ def find_min_max(*args):
 
     return (min_num, max_num)
 
-# TO DO: Write test cases and pass them to the function.
+# TODO: write test cases and pass them to the function. Print the results
+# --- Test Cases ---
 
-# Manual test cases
-print("Manual Test Cases:")
+print("Test Cases:")
 print(f"find_min_max(4, 7, 2, 9, 0, 3) = {find_min_max(4, 7, 2, 9, 0, 3)}")
 print(f"find_min_max(-9, -12, 38, 489, 38, 2, -2) = {find_min_max(-9, -12, 38, 489, 38, 2, -2)}")
 print(f"find_min_max(3903, 28, math.e, -90) = {find_min_max(3903, 28, math.e, -90)}")
