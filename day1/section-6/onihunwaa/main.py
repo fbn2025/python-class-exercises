162b5b450b2f3195b83bcdd42cc91c62f7151856
Onihunwa Augustine <onihunwaa@gmail.com>
2025-08-30 19:52:00 +0100

    Update main.py
    updated version

--- day1/section-6/onihunwaa/main.py
+++ day1/section-6/onihunwaa/main.py
@@ -1,10 +1,12 @@
 import math
 
 def find_min_max(*args):
-    if not args:
-        raise ValueError("At least one number is required")
-    return (min(args), max(args))
+    try:
+      return f'In the list {args}, \n \t the minimum value is {min(args)}, and maximum value is {max(args)}'
+    except ValueError:
+        return "ValueError encountered: No value was supplied."
 
-print(find_min_max(4, 7, 2, 9, 0, 3))                 
-print(find_min_max(-9, -12, 38, 489, 38, 2, -2))      
-print(find_min_max(3903, 28, math.e, -90))            
+print(find_min_max())
+print(find_min_max(4, 7, 2, 9, 0, 3))
+print(find_min_max(-9, -12, 38, 489, 38, 2, -2))
+print(find_min_max(3903, 28, math.e, -90))
