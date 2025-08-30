a7c1eee740066b6d8dea80e03d4a28f4fb565b85
badmusibrahimadewale01-dot <badmusibrahimadewale01@gmail.com>
2025-08-30 08:45:44 +0100

    Completed Exercise 1

--- day1/section-2/badmusibrahimadewale01/main.py
+++ day1/section-2/badmusibrahimadewale01/main.py
@@ -1,18 +1,17 @@
 my_internet_profile = {
-    "name": None,                          # string
-    "twitter_handle": None,                # string
-    "favorite_physics_constant": None,     # float
-    "age": None,                           # integer (you can lie about this lol)
-    "finished_uni": None,                  # boolean
-    "hobbies": None,                       # tuple
-    "skills": None,                        # list
-    "personal_quotes": None,               # set (one or two)
+    "name": "Badmus",                          # string
+    "twitter_handle": "Harderwaleysneh",                # string
+    "favorite_physics_constant": 6.62607015e-34,     # float
+    "age": 300,                           # integer (you can lie about this lol)
+    "finished_uni": True,                  # boolean
+    "hobbies": ("Gaming", "Music", "Movies"),                       # tuple
+    "skills": ["Communication", "Eating", "Sleeping"],                        # list
+    "personal_quotes": set(["Live and let live", "To be or not to be"]),               # set (one or two)
     "contact_info": {                      # dictionary (nested)
-        "phone_number": None,              # integer (you can put a fake one)
-        "email": None,                     # string (you can put a fake one)
-        "website": None,                   # string
+        "phone_number": 2348144846040,              # integer (you can put a fake one)
+        "email": "badmusibrahimadewale01@gmail.com",                     # string (you can put a fake one)
+        "website": "iDANgangan.ng",                   # string
     },
 }
 
 print(my_internet_profile)
-
