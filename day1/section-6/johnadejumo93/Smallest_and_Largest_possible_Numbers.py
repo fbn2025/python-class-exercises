a0a16072aa777e987ec0b2eee21867aa85c5232a
John-adejumo <johnadejumo94@gmail.com>
2025-08-30 22:06:45 +0100

    Update Smallest_and_Largest_possible_Numbers.py

--- day1/section-6/johnadejumo93/Smallest_and_Largest_possible_Numbers.py
+++ day1/section-6/johnadejumo93/Smallest_and_Largest_possible_Numbers.py
@@ -1,11 +1,3 @@
-def find_min_max(*args):
-  min_num = float("-inf") # smallest possible number in python
-  max_num = float("inf") # largest possible number in python
-  
-  return (min_num, max_num)
-
-# TODO: write test cases and pass them to the function. Print the results
-
 import math
 
 def find_min_max(*args):
@@ -20,6 +12,7 @@ def find_min_max(*args):
     
     return (min_num, max_num)
   
+# TODO: write test cases and pass them to the function. Print the results  
 print(find_min_max(4, 7, 2, 9, 0, 3))      
 
 print(find_min_max(-9, -12, 38, 489, 38, 2, -2))  
