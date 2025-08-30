8fd6751c356485351d4036237e3e87e644833760
heisreagan <133244435+heisreagan@users.noreply.github.com>
2025-08-30 09:55:31 +0100

    For Loop and While Loop.py
    - Added a for loop to search for a user-provided account number within a predefined list.
      - Iterates through the list of existing accounts.
      - Uses a flag (`found`) to determine if the account exists.
      - Provides appropriate user feedback ("Account found!" or "Account not found.").
    
    - Implemented a while loop to validate user input for a 10-digit account number.
      - Ensures input contains only digits and is exactly 10 characters long.
      - Continuously prompts the user until valid input is provided.
      - Prevents invalid data from being accepted or processed further.

--- day1/section-5/ijeomareaganok/main.py
+++ day1/section-5/ijeomareaganok/main.py
@@ -1,6 +1,37 @@
+# Exercise 1 - For Loop
+accounts = ["1234567890", "9876543210", "5555555555", "1111222233"]
+
+account_to_search = input("Enter account number to search: ")
+
+# TODO: use a for loop to check if search_account is in accounts
+# Hint: loop through accounts and compare the `account_to_search` to the looped element 
+
+# solution
+found = False
+
+for account in accounts: 
+    if account_to_search == account:
+        found = True
+        break    # (Stop the loop once we find the account)
+    
+if found:
+    print("Account found!")
+else:
+    print("Account not found!")
+
+
+
+# Exercise 1 - While Loop
 while True:
     account_number = input("Enter your 10-digit account number: ")
 
     # TODO: check that it is all digits and has length 10
     # If valid -> break loop, else keep asking
+#solution
+
+    if account_number.isdigit() and len(account_number) == 10:
+        print("Valid account number.")
+        break
+    else:
+        print("Invalid account number, please enter exactly 10 digits")
 
