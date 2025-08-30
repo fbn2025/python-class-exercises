06d75d8d59ab8b5eea88bb9e721013054451707f
John-adejumo <johnadejumo94@gmail.com>
2025-08-30 21:25:41 +0100

    Update CalculatorHistory.py

---
+++ day1/section-7/johnadejumo93/CalculatorHistory.py
@@ -0,0 +1,86 @@
+# Practice Keeping State
+
+# Task: implement a new class called CalculatorHistory, then initialize it within the __init__ method of the calculator class. 
+# Purpose: To make it possible to use object accessors to access history methods rather than dictionary accessors
+
+ # goal:  To make self.history.addition.append((num1, num2, result))
+
+class CalculatorHistory:
+    def __init__(self):
+        self.addition = []
+        self.subtraction = []
+        self.multiplication = []
+        self.division = []
+
+    def __str__(self):
+        return (f"""
+            Addition: {self.addition}\n
+            Subtraction: {self.subtraction}\n
+            Multiplication: {self.multiplication}\n
+            Division: {self.division}
+        """
+        )
+
+
+class Calculator:
+    def __init__(self, log_results: bool = False):
+        self.log_results = log_results
+        self.history = CalculatorHistory()
+
+
+    def add(self, num1: float, num2: float):
+        result = num1 + num2
+
+        if self.log_results:
+            print(f"The sum of {num1} and {num2} is {result}")
+        self.history.addition.append((num1, num2, result))
+        
+        return result
+    
+
+    def subtract(self, num1: float, num2: float):
+        result = num1 - num2
+
+        if self.log_results:
+            print(f"The difference between {num1} and {num2} is {result}")
+        self.history.subtraction.append((num1, num2, result))
+        
+        return result
+    
+
+    def multiply(self, num1: float, num2: float):
+        result = num1 * num2
+
+        if self.log_results:
+            print(f"The product of {num1} and {num2} is {result}")
+        self.history.multiplication.append((num1, num2, result))
+       
+        return result
+    
+
+    def divide(self, num1: float, num2: float):
+        if num2 == 0:
+
+            raise ZeroDivisionError("Division by zero is not allowed.")
+        
+        result = num1 / num2
+
+        if self.log_results:
+            print(f"The division of {num1} by {num2} is {result}")
+        self.history.division.append((num1, num2, result))
+
+        return result
+    
+    
+
+calculator = Calculator(log_results=True)
+
+calculator.add(4, 5)
+calculator.subtract(10, 7)
+calculator.multiply(6, 8)
+calculator.divide(20, 5)
+
+print("\n--- History ---")
+print("The Calculator history is", calculator.history)
+
+
