d62f6397cc20f9bd287b6fe3441a21a2f17e1b66
Akintayo abidemi tope <akintayoabidemitope@gmail.com>
2025-08-30 06:31:32 -0700

    Add changes to file

--- day1/section-6/akintayoabidemitope/main.py
+++ day1/section-6/akintayoabidemitope/main.py
@@ -1,17 +1,19 @@
 import math
-def find_min_max(*args):
-    min_num = float("inf")   # very large number
-    max_num = float("-inf")  # very small number
 
-    for num in args:
+def find_min_max(*numbers):
+    if not numbers:
+        return None 
+
+    min_num = max_num = numbers[0]
+    for num in numbers[1:]:
         if num < min_num:
             min_num = num
-        if num > max_num:
+        elif num > max_num:
             max_num = num
 
     return (min_num, max_num)
 
-# Test cases
-print(find_min_max(4, 7, 2, 9, 0, 3))              # Expected (0, 9)
-print(find_min_max(-9, -12, 38, 489, 38, 2, -2))  # Expected (-12, 489)
-print(find_min_max(3903, 28, math.e, -90))        # Expected (-90, 3903))
+# Test Cases
+print(find_min_max(4, 7, 2, 9, 0, 3))  # (0, 9)
+print(find_min_max(-9, -12, 38, 489, 38, 2, -2))  # (-12, 489)
+print(find_min_max(3903, 28, math.e, -90))  # (-90, 3903)
