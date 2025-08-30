1b01d22bd019bbb3ea5656961d4d0960cf418d5f
badmusibrahimadewale01-dot <badmusibrahimadewale01@gmail.com>
2025-08-30 08:47:53 +0100

    Completed Exercise 2

--- day1/section-3/badmusibrahimadewale01/main.py
+++ day1/section-3/badmusibrahimadewale01/main.py
@@ -1,4 +1,12 @@
-score = int(input("Enter your exam score (0-100): "))
+score = float(input("Enter your exam score (0-100): "))
 
-# write your if/elif/else statements here
-
+if score >= 70:
+    print("A")
+elif score >= 60:
+    print("B")
+elif score >= 50:
+    print("C")
+elif score >= 40:
+    print("D")
+else:
+    print("F")
