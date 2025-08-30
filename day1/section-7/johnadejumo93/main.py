6c32f3b5f93ac91a3dc4612dd249a0c811297406
John-adejumo <johnadejumo94@gmail.com>
2025-08-30 23:09:44 +0100

    Update main.py

--- day1/section-7/johnadejumo93/main.py
+++ day1/section-7/johnadejumo93/main.py
@@ -1,5 +1,4 @@
 # Practice Keeping State
-
 # Task: implement a new class called CalculatorHistory, then initialize it within the __init__ method of the calculator class. 
 # Purpose: To make it possible to use object accessors to access history methods rather than dictionary accessors
 
