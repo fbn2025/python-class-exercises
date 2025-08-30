85b67f5845b20c453bb84d92293f6eb5343700a6
Juliet Ucheoma Osuagwu <osuagwuucheoma@gmail.com>
2025-08-30 23:32:37 +0100

    Add import math for the third test case

--- day1/section-6/osuagwuucheoma/main.py
+++ day1/section-6/osuagwuucheoma/main.py
@@ -1,3 +1,4 @@
+import math
 def find_min_max(*args):
   min_num = float("inf") # largest possible number in python
   max_num = float("-inf") # smallest possible number in python
@@ -10,6 +11,7 @@ def find_min_max(*args):
       
 print (find_min_max(4, 7, 2, 9, 0, 3))
 print (find_min_max(-9, -12, 38, 489, 38, 2, -2))
+print (find_min_max(3903, 28, math.e, -90))
 
 # TODO: write test cases and pass them to the function. Print the results
 
