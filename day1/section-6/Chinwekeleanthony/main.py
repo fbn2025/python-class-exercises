986060161e9c3a3c96a749dd040443c9a6726a27
Chinwekele Charles Anthony <chinwekeleanthony@gmail.com>
2025-08-30 18:35:11 +0100

    Update main.py

--- day1/section-6/Chinwekeleanthony/main.py
+++ day1/section-6/Chinwekeleanthony/main.py
@@ -1,21 +1,20 @@
-def find_min_max(*args):
-    if not args:
-        return None  # handle case when no numbers are passed
 
-    min_num = float("inf")   # start with largest possible number
-    max_num = float("-inf")  # start with smallest possible number
+import math
 
-    for num in args:
+def find_min_max(*numbers):
+    if not numbers:
+        return None 
+
+    min_num = max_num = numbers[0]
+    for num in numbers[1:]:
         if num < min_num:
             min_num = num
-        if num > max_num:
+        elif num > max_num:
             max_num = num
 
     return (min_num, max_num)
 
 
-# Test cases
-print(find_min_max(4, 7, 2, 9, 0, 3))                 
-print(find_min_max(-9, -12, 38, 489, 38, 2, -2))     
-import math
-print(find_min_max(3903, 28, math.e, -90))          
+print(find_min_max(4, 7, 2, 9, 0, 3))  # (0, 9)
+print(find_min_max(-9, -12, 38, 489, 38, 2, -2))  # (-12, 489)
+print(find_min_max(3903, 28, math.e, -90))  # (-90, 3903)
