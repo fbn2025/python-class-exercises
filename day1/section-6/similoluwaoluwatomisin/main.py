a871597b4613dc209fff436c69d9b690b6ca9881
RelyOnGod <148478541+Tomi-Dev1@users.noreply.github.com>
2025-08-30 12:44:21 +0100

    Update main.py

--- day1/section-6/similoluwaoluwatomisin/main.py
+++ day1/section-6/similoluwaoluwatomisin/main.py
@@ -14,17 +14,23 @@
 # TODO: write test cases and pass them to the function. Print the results
 
 import math
+
 def find_min_max(*args):
-    if not args:
-        raise ValueError("At least one number is required")
-    min_num = float("inf")  # largest possible number in python
-    max_num = float("-inf") # smallest possible number in python
-    for x in args:
-        if x < min_num:
-            min_num = x
-        if x > max_num:
-            max_num = x
-    return (min_num, max_num)
+  min_num = float("-inf") # smallest possible number in python
+  max_num = float("inf") # largest possible number in python
+
+  if not args:
+     return (min_num, max_num)
+
+  for number in args:
+    if number > min_num:
+      min_num = number
+    if number < max_num:
+      max_num = number
+
+  min_num, max_num = max_num, min_num
+
+  return (min_num, max_num)
 
 print(find_min_max(4, 7, 2, 9, 0, 3))                 # -> (0, 9)
 print(find_min_max(-9, -12, 38, 489, 38, 2, -2))      # -> (-12, 489)
