3c8c23ab233f057887e046b9da883512a962e493
FRIDAY EZEKIEL <fridayks96@gmail.com>
2025-08-30 10:47:13 +0100

    Update main.py

--- day1/section-7/fridayks96/main.py
+++ day1/section-7/fridayks96/main.py
@@ -1,19 +1,38 @@
+
+## Practice Keeping State
+
+# Task: implement a new class called CalculatorHistory, then initialize it within the __init__ method of the calculator class.
+# Purpose: To make it possible to use object accessors to access history methods rather than dictionary accessors
+# Starting point:
+
 class CalculatorHistory:
     def __init__(self):
         # add the remaining
         self.addition = []
+        self.subtraction = []
+        self.multiplication = []
+        self.division = []
 
     def __str__(self):
         return f"""
             Addition: {self.addition}
+            Subtraction: {self.subtraction}
+            Multiplication: {self.multiplication}
+            Division: {self.division}
         """
 
 class Calculator:
     def __init__(self, log_results: bool = False):
         self.log_results = log_results
+        """
         self.history = {
-            "addition": []
+            "addition": [],
+            "subtraction": [],
+            "multiplication": [],
+            "division": []
         }
+        """
+        self.history = CalculatorHistory()
 
     def add(self, num1: float, num2: float):
         result = num1 + num2
@@ -21,18 +40,50 @@ class Calculator:
         if self.log_results:
             print(f"The sum of {num1} and {num2} is {result}")
 
-        self.history["addition"].append((num1, num2, result))
-        # goal 
-        # self.history.addition.append((num1, num2, result))
+        # goal
+        self.history.addition.append((num1, num2, result))
 
         return result
 
+    def subtract(self, num1: float, num2: float):
+        result = num1 - num2
 
-calculator = Calculator()
+        if self.log_results:
+            print(f"The difference of {num1} and {num2} is {result}")
 
+        # goal
+        self.history.subtraction.append((num1, num2, result))
+
+        return result
+
+    def multiply(self, num1: float, num2: float):
+        result = num1 * num2
+
+        if self.log_results:
+            print(f"The multiplication of {num1} and {num2} is {result}")
+
+        # goal
+        self.history.multiplication.append((num1, num2, result))
+
+        return result
+
+    def divide(self, num1: float, num2: float):
+        try:
+            result = num1 / num2
+        except ZeroDivisionError:
+            print(f"Error: Cannot divide by zero!")
+
+        if self.log_results:
+            print(f"The division of {num1} and {num2} is {result}")
+
+        # goal
+        self.history.division.append((num1, num2, result))
+
+        return result
+
+calculator = Calculator(log_results=True)
 calculator.add(4, 5)
-calculator.add(20, 25)
-calculator.add(-20, 30)
-
-print(calculator.history)
+calculator.subtract(10, 3)
+calculator.multiply(2, 6)
+calculator.divide(8, 2)
 
