11a17c6f0ff49205b1feb9db427f4f8c102c73a7
Egunjobi Ayooluwa Oluwaferanmi <egunjobiayooluwa@gmail.com>
2025-08-30 17:34:50 +0100

    Update 8th exercise into section 8 main.py

--- day1/section-8/egunjobiayooluwa/main.py
+++ day1/section-8/egunjobiayooluwa/main.py
@@ -1,20 +1,59 @@
-class BankAccount:
-    def __init__(self, name, balance=0):
-        self.name = name
-        self.balance = balance
+from abc import ABC, abstractmethod
+
+class AbstractBankAccount(ABC):
+    @abstractmethod
+    def deposit(self, amount):
+        pass
+
+    @abstractmethod
+    def withdraw(self, amount):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+class BankAccount(AbstractBankAccount):
+    def __init__(self, customer_name, balance=0):
+         self.customer_name = customer_name
+         self.balance = balance
 
     def deposit(self, amount):
-        pass  # implement this
+        if amount <= 0:
+            print("Deposit amount must be greater than zero (0)")
+        else:
+            self.balance += amount
+            print(f"Amount deposited: {amount}. New balance: {self.balance}.")
 
     def withdraw(self, amount):
-        pass  # implement this
+        if amount > self.balance:
+            print("Insufficient funds!")
+        elif amount <= 0:
+            print("Withdrawal amount must be greater than zero (0)")
+        else:
+            self.balance -= amount
+            print(f"Amount withdrawn: {amount}. New balance: {self.balance}.")
 
     def __str__(self):
-        return f"{self.name} has a balance of {self.balance}"
+        return (f"Account holder: {self.customer_name}. \tBalance: {self.balance}")
 
-# TODO: Create a SavingsAccount class that inherits from BankAccount
+class SavingsAccount(BankAccount):
+    def __init__(self, customer_name, balance=0):
+        super().__init__(customer_name, balance)
 
-# Example usage
-account = BankAccount("Alice", 1000)
-print(account)
+    def add_interest(self, interest_rate):
+        if interest_rate < 0:
+            print("Interest rate must be positive")
+        else:
+            earned_interest = self.balance * (interest_rate / 100)
+            self.balance += earned_interest
+            print(f"Accrued interest: {earned_interest} @{interest_rate}% pa. New balance: {self.balance}")
 
+
+savings = SavingsAccount("Ayoose", 2000)
+print(f"\tWelcome to ABC Bank \n{savings}")
+savings.deposit(1500)
+savings.withdraw(500)
+savings.add_interest(10)
+
+print(savings)
