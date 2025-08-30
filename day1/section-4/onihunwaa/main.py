141144feda680de9b58f315be3c4c0bbefbf439f
Onihunwa Augustine <onihunwaa@gmail.com>
2025-08-30 19:43:06 +0100

    Update main.py
    updated

--- day1/section-4/onihunwaa/main.py
+++ day1/section-4/onihunwaa/main.py
@@ -1,8 +1,6 @@
-# write your if/elif/else statements here
-# Ask the user for their exam score
-score = int(input("Enter your exam score (0–100): "))
 
-# Determine the grade using conditionals
+score = int(input("Enter your exam score (0 – 100): "))
+
 if score >= 70:
     grade = "A"
 elif score >= 60:
@@ -13,9 +11,7 @@ elif score >= 40:
     grade = "D"
 else:
     grade = "F"
-# Print the result
+    
 print(f"Your grade is: {grade}")
 
-# TODO: use a for loop to check if search_account is in accounts
-# Hint: loop through accounts and compare the `account_to_search` to the looped element
 
