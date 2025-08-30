0f72a46ed83e452ccb87eee410c7aa2d8eac4445
Simeon Shodeinde <shodeindesimeon@gmail.com>
2025-08-30 20:23:12 +0100

    Complete Exercise 2

--- day1/section-3/Shodeindesimeon/main.py
+++ day1/section-3/Shodeindesimeon/main.py
@@ -1,10 +1,10 @@
 name = input("What is your name: ")
-age = input("What about your age? ")
+age = input("How old are you? ")
 interest1 = input("What is your number 1 interest: ")
 interest2 = input("What is your number 2 interest: ")
 interest3 = input("What is your number 3 interest: ")
 
-combined = f"Your name is {name}, you are {age} and your interests are {interest1}, {interest2}, {interest3}"
+combined = f"Your name is {name}, you are {age} and your interests are {interest1}, {interest2}, and {interest3}"
 
 print(combined)
 
