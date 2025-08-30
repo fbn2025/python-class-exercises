dc6101cb96b0631fa7b119a92cd01a490df8ffcb
Oluwatobi3126 <159538568+Oluwatobi3126@users.noreply.github.com>
2025-08-30 06:30:48 +0100

    Update main.py

--- day1/section-6/ijeteoluwatobi/main.py
+++ day1/section-6/ijeteoluwatobi/main.py
@@ -1,3 +1,5 @@
+import math
+
 def find_min_max(*args):
     if not args:
         return None  # handle case when no numbers are passed
@@ -17,6 +19,6 @@ def find_min_max(*args):
 # Test cases
 print(find_min_max(4, 7, 2, 9, 0, 3))                 # (0, 9)
 print(find_min_max(-9, -12, 38, 489, 38, 2, -2))      # (-12, 489)
-import math
 print(find_min_max(3903, 28, math.e, -90))            # (-90, 3903)
+
 
